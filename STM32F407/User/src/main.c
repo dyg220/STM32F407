28f67e16d65722df7c69db9d285097479d8d8de0
dyg220 <1503639251@qq.com>
2025-08-01 11:35:12 +0800

    add

--- STM32F407/User/src/main.c
+++ STM32F407/User/src/main.c
@@ -6,10 +6,11 @@ int main(void)
 	u8 key_num = 0;
 	u8 speed = 5;
 	u8 led_flag = 0;
+	u8 data = 0;
+	USART1_Init(115200);
 	LED_Init();
 	Beep_Init();
 	Fire_Init();
-	USART1_Init(115200);
 	//BEEP_ON;
 	//delay_ms(300);
 	//BEEP_OFF;
@@ -23,41 +24,48 @@ int main(void)
 			{
 				LED1_OFF;
 			}*/
-		/*key_num = Key_Scan();
-		if (key_num == 1)
-		{
-			led_flag = 1;
-		}
-		if (key_num == 2)
-		{
-			led_flag = 0;
-		}
-		if ((key_num == 3) && led_flag)
-		{
-			speed--;
-			if (speed < 1)
-				speed = 1;
-		}
-		if ((key_num == 4) && led_flag)
-		{
-			speed++;
-			if (speed > 10)
-				speed = 10;
-		}
-		if (led_flag == 1)
-		{
-			LED_Water(speed);
-		}
-		if (led_flag == 0)
-		{
-			LED_ALLOFF();
-		}*/
+			/*key_num = Key_Scan();
+				if (key_num == 1)
+				{
+					led_flag = 1;
+				}
+				if (key_num == 2)
+				{
+					led_flag = 0;
+				}
+				if ((key_num == 3) && led_flag)
+				{
+					speed--;
+					if (speed < 1)
+						speed = 1;
+				}
+				if ((key_num == 4) && led_flag)
+				{
+					speed++;
+					if (speed > 10)
+						speed = 10;
+				}
+				if (led_flag == 1)
+				{
+					LED_Water(speed);
+				}
+				if (led_flag == 0)
+				{
+					LED_ALLOFF();
+				}*/
 
-		USART1_SendByte('A');
+		data = USART1_RecvByte();
+		if (data == 'O')
+		{
+			LED1_ON;
+
+		}
+		if (data == 'N')
+		{
+			LED1_OFF;
+		}
 		delay_ms(500);
 
-
-		
 	}
 	return 0;
 }
